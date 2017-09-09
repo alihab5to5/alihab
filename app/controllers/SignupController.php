e0c15eba39944c6de1014f3abfba359d9697cd69
dmitriy <alihab5to5@gmail.com>
2017-09-09 12:08:08 +0300

    set phalcon

---
+++ app/controllers/SignupController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmitriy
+ * Date: 03.09.17
+ * Time: 20:57
+ */
--- index.php
+++ index.php
@@ -1,11 +1,2 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: dmitriy
- * Date: 13.08.17
- * Time: 2:10
- */
-
-echo 'hello word';
-echo '<br>';
-echo 'hello word';
+include_once 'public/index.php';
